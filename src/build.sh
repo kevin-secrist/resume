f4e917850e811f327bf503a4eed8923d2addc319
Kevin Secrist <kevin@secrist.dev>
2023-08-09 23:44:10 -0400

    Move out directory to root

--- src/build.sh
+++ src/build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-latexmk -synctex=1 -interaction=nonstopmode -file-line-error -lualatex -outdir=/data/src/out /data/src/cv
+latexmk -synctex=1 -interaction=nonstopmode -file-line-error -lualatex -outdir=/data/out /data/src/secrist-resume
+latexmk -synctex=1 -interaction=nonstopmode -file-line-error -lualatex -outdir=/data/out /data/src/secrist-cv
